433d38ae42e11e037bd8bcf7c26889b73c064326
Darkzwer123 <137776351+Darkzwer123@users.noreply.github.com>
2024-06-06 11:05:08 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,10 +9,6 @@ let package = Package(
             targets: ["RegulaCommonSwift"]),
     ],
     targets: [
-        .binaryTarget(name: "RegulaCommonSwift", url: "https://pods.regulaforensics.com/RegulaCommon/7.1.379/RegulaCommon-7.1.379.zip", checksum: "1085a24689c471ad470b83d7d568a53d7ac1dbb5d905628c82d4513a67dd5049"),
+        .binaryTarget(name: "RegulaCommonSwift", url: "https://pods.regulaforensics.com/RegulaCommon/7.2.487/RegulaCommon-7.2.487.zip", checksum: "79e401a8cfc0a16751479bccfca645709037e0afd6c78f653b765fc846ad0220"),
     ]
 )
-
-//4a8371633dee88d9e7b4ca111a14ae83eb817b895a59cb0a30a77e7b5fd4df50
-//1085a24689c471ad470b83d7d568a53d7ac1dbb5d905628c82d4513a67dd5049
-//
